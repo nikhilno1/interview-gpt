38adb57235c94819d52f1a1ee9c00d089c6ab833
Nikhil <nikhil.utane@gmail.com>
2023-11-09 07:54:18 +0000

    improved evaluation prompt

--- config.py
+++ config.py
@@ -25,23 +25,31 @@ Answer:
 
 EVALUATE_SYSTEM_PROMPT = """
 You are an expert at evaluating answers given to behaviorial questions in interviews.  
-You need to analyze given question and answer and give comprehensive feedback to the user on:
-1. The clarity and structure of the answer.
-2. How well the user demonstrated the relevant skills and experiences for the job.
-3. The appropriateness of the examples the user used.
-4. My communication style and whether it was engaging.
-5. Ways to make the answer more concise without losing important details.
-6. How the user could better align response with the STAR method (Situation, Task, Action, Result).
-7. Any other tips to improve storytelling and to make a stronger impression on the interviewers.
-8. If there is a reference answer also given, then you should evaluate how close the reference and given answers are and include that information in your analysis.
+You need to analyze given question and answer and give comprehensive, helpful and concise feedback to the user.
+Be strict in your evaluation and have a high bar.
+The feedback needs to be given in a tabular format with rows being the evaluation criteria heading, first column being the rating for it and second column a short reason why the specific rating was given.
+The rating needs to be given as "Needs improvement", "OK", "Great".
+Evaluation criteria:
+1. Match with reference: Similarity with reference answer. Mention NA if no reference answer was provided. Check if everything given in the reference answer was covered. Mention the % match.
+2. Clarity: The clarity and structure of the answer. Check if the answer follows the STAR format wherever feasible. 
+3. Skills Demonstration: How well the user demonstrated the relevant skills and experiences for the job. Ensure that the candidate talked about the skills that would be expected of his position.
+4. Examples appropriateness: The appropriateness of the examples the user used. Mention NA if no example given.
+5. Communication Style: The communication style and whether it was engaging. Check if the answer was appropriate for the position applied.
+6. Filler words: Usage of filler words such as you know, like, um, ah.
+
+Below the table also provide these tips to improve the answer:
+1. Ways to make the answer more concise without losing important details.
+2. How the user could better align response with the STAR method (Situation, Task, Action, Result).
+3. Any other tips to improve storytelling and to make a stronger impression on the interviewers.
+
 """
 
 EVALUATE_USER_PROMPT = """
-I have recently had an interview where I was asked the following behavioral question: {QUESTION_HERE}. Below is the response I provided: {ACTUAL_ANSWER}. 
+I have recently had an interview for {JOB_TITLE} position where I was asked the following behavioral question: {QUESTION_HERE}. Below is the response I provided: {ACTUAL_ANSWER}. 
 """
 
 REFERENCE_ANSWER_PROMPT = """
-However for reference this is the ideal answer I would have liked to provide:
+However this is the ideal or reference answer I would have liked to provide:
 {REFERENCE_ANSWER_HERE}
 """
 
--- evaluate_answer.py
+++ evaluate_answer.py
@@ -2,6 +2,11 @@ import streamlit as st
 import openai
 import os
 from config import CHAT_MODEL, EVALUATE_SYSTEM_PROMPT, EVALUATE_USER_PROMPT, REFERENCE_ANSWER_PROMPT, ALL_FILE_NAMES, QNA_FOLDER
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".env.local")
+user_job_title = os.getenv("USER_JOB_TITLE", "Senior Software Engineer")
+
 
 def get_qna_content(selected_option, file_name):
     # Construct the full path to the file
@@ -24,7 +29,7 @@ def evaluation_result(actual_answer, selected_option):
         reference_answer = get_qna_content(selected_option, ALL_FILE_NAMES[3])
 
         evaluate_answer_prepped = EVALUATE_USER_PROMPT.format(
-            QUESTION_HERE=question, ACTUAL_ANSWER=actual_answer
+            JOB_TITLE=user_job_title, QUESTION_HERE=question, ACTUAL_ANSWER=actual_answer
         )
         if len(reference_answer) > 10:
             evaluate_answer_prepped += REFERENCE_ANSWER_PROMPT.format(REFERENCE_ANSWER_HERE=reference_answer)
