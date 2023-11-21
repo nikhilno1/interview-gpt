977e4d7ae047c3e16dd8552063ee122636f8223c
Shaurya Utane <shaurya.utane@gmail.com>
2023-11-21 22:51:37 +0530

    fixed various issues - first provisionally working version

--- interview_gpt.py
+++ interview_gpt.py
@@ -11,13 +11,55 @@ from generate_answers import generate_chatgpt_answer, qna_dict, read_qna_dict_fr
 question_data = [(item.split('|')[0].strip(), item.split('|')[-1].strip()) for item in QUESTIONS_DATA]
 question_data.sort(key=lambda pair: pair[1])
 
-def read_qna_data():
-    print("inside read_qna_data - to be implemented")
+def read_qna_data(single_folder=""):    
+    global qna_dict
+    question = rough_answer = chatgpt_answer = ""
+    for root, dirs, files in os.walk(QNA_FOLDER):
+        if dirs == []:
+            continue        
+        if single_folder != "":
+            # Keep only the passed folder name and remove all other elements
+            dirs = [element for element in dirs if element == single_folder]
+            #print("dirs updated to ", dirs)
+
+        if "sample" in dirs:
+            dirs.remove("sample")
+
+        for dir in dirs:
+            file_path =  os.path.join(root, dir) + '/'
+            with open( file_path + ALL_FILE_NAMES[0], 'r') as file:
+                question = file.read()                
+                
+            with open( file_path + ALL_FILE_NAMES[1], 'r') as file:
+                rough_answer = file.read()
+
+            with open( file_path + ALL_FILE_NAMES[2], 'r') as file:
+                chatgpt_answer = file.read()
+
+            with open( file_path + ALL_FILE_NAMES[3], 'r') as file:
+                final_answer = file.read()    
+
+            # update the dictionary
+            json_data = {
+                "question": question,
+                "rough_answer": rough_answer,
+                "chatgpt_answer": chatgpt_answer,
+                "final_answer": final_answer
+            }
+            qna_dict[dir] = json.dumps(json_data)     
+            #print("qna_dict updated for ", qna_dict[dir])       
+    print("qna_dict generated")        
 
 def init_session_state():
     """Initialize the session state variables."""
+    if 'initialization_done' not in st.session_state:
+        st.session_state.initialization_done = False
+
     if 'selected_question' not in st.session_state:
         st.session_state.selected_question = None
+
+    if 'prev_selected_question' not in st.session_state:
+        st.session_state.prev_selected_question = None
 
     if 'random_button_pressed' not in st.session_state:
         st.session_state.random_button_pressed = False
@@ -43,6 +85,13 @@ def display_sidebar(options):
             selected_option = st.radio("Select a question", options, index=0)
             idx = options.index(selected_option)
             st.session_state.selected_question = question_data[idx][0]
+            if selected_option != st.session_state.prev_selected_question:
+                st.session_state.prev_selected_question = selected_option
+                st.session_state.rough_answer_text = "" 
+                st.session_state.chatgpt_answer_text = ""
+                st.session_state.final_answer_text = ""
+                #print("Resetted all state variables")
+
 
 def get_selected_folder_from_question(question):
     option = [pair[1] for pair in question_data if pair[0] == question][0]    
@@ -72,25 +121,29 @@ def save_answer_to_file(directory, folder, filename, content=""):
     file_path =  directory + '/' + folder + '/' + filename 
     print("Saving to file ", file_path)
     with open(file_path, 'w') as file:
-        print("content, ", content)
+        #print("content, ", content)
         file.write(content)
+    # update dictionary as well for the saved folder
+    read_qna_data(folder) 
 
 def display_qna_widgets():
     #global qna_dict
 
     selected_option = get_selected_folder_from_question(st.session_state.selected_question)
-    if(qna_dict == {}):
-        read_qna_dict_from_file()
+    #if(qna_dict == {}):
+    #    read_qna_dict_from_file()
     
     if(selected_option in qna_dict):
         #st.write(f'First action for: {selected_option}')    
         question_data = json.loads(qna_dict[selected_option])
+        #print(question_data)
         
         st.session_state.rough_answer_text = question_data["rough_answer"]        
         if "chatgpt_answer" in question_data and st.session_state.chatgpt_answer_text == "":
             st.session_state.chatgpt_answer_text = question_data["chatgpt_answer"]            
         if "final_answer" in question_data and st.session_state.final_answer_text == "":    
             st.session_state.final_answer_text = question_data["final_answer"]
+            #print("final_answer_text ", st.session_state.final_answer_text)
 
     # =========================================================================================== #
     rough_answer = st.text_area("Enter your rough answer:", value=st.session_state.rough_answer_text, height=200)
@@ -139,7 +192,7 @@ def display_qna_widgets():
     #print("3 final_answer is ", final_answer)
     if final_answer_submit:
         st.session_state.final_answer_text = final_answer    
-        st.write(f'Updated final answer: {final_answer}')    
+        #st.write(f'Updated final answer: {final_answer}')    
         #print("4 final_answer is ", final_answer)
 
     if final_answer_save:
@@ -176,13 +229,14 @@ def display_main_content(questions):
 
 # Main application logic
 def main():
-    # Read the questions data and create qna folders for first time
-    create_folders_for_questions()
-    read_qna_data()
-
-    st.title("Welcome to Interview-GPT")
     init_session_state()    
-    
+    # Read the questions data and create qna folders for first time
+    if st.session_state.initialization_done is False:
+        create_folders_for_questions()    
+        read_qna_data()
+        st.session_state.initialization_done = True
+
+    st.title("Welcome to Interview-GPT")       
     _, extracted_words = zip(*question_data)
     display_sidebar(extracted_words)
 
