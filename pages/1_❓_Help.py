b2146e77b796d7479b09e75dd6e30514412046e6
Nikhil <nikhil.utane@gmail.com>
2023-12-22 07:20:23 +0000

    updated help

--- pages/1_❓_Help.py
+++ pages/1_❓_Help.py
@@ -7,19 +7,21 @@ st.markdown(
                 
         #### Usage
 
-        Step 1 (Optional) - Provide the details about the position you are interviewing for such as Job Title, Years of Experience and Interview Company name 
+        Step 1 (Optional) - Login using your google account to save your answers else continue as guest.
 
-        Step 2 - Provide a rough answer to the prompted question and then click Submit to get ChatGPT refined answer.
+        Step 2 (Optional) - Provide the details about the position you are interviewing for such as Job Title, Years of Experience and Interview Company name.
 
-        Step 3 - Select Copy to copy the answer as your final answer and make suitable edits to it. Once done click Submit.
+        Step 3 - Provide a rough answer to the prompted question and then click Submit to get ChatGPT refined answer.
 
-        Step 4 - Click the Mic icon to start recording your answer.
+        Step 4 - Select Copy to copy the answer as your final answer and make suitable edits to it. Once done click Submit.
 
-        Step 5 - Click the Mic icon again to stop recording once done.
+        Step 5 - Click the Mic icon to start recording your answer.
 
-        Step 6 - Click Analyze to view the detailed analysis of your answer.
+        Step 6 - Click the Mic icon again to stop recording once done.
 
-        Step 7 - Select another question from the sidebar or click "Pick a Random Question" to practice another question.
+        Step 7 - Click Analyze to view the detailed analysis of your answer.
+
+        Step 8 - Select another question from the sidebar or click "Pick a Random Question" to practice another question.
 
         #### [Feedback](https://forms.gle/knzJawEDyPBMj1bx5)
     
