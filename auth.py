bd847b1eb5766886054a497eac9364a30faaaab4
Abubakr Chan <89600478+AbubakrChan@users.noreply.github.com>
2024-04-13 18:01:03 +0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -46,7 +46,7 @@ def authenticate_user():
     #handling cookies here
     # value = get_email()
     cookie_manager = get_manager()
-    st.write(cookie_manager)
+    st.write(len(cookie_manager))
     # st.write(cookie_manager)
     # cookies = cookie_manager.get_all()
     
